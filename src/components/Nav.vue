e508faf2d79f57e1572453843a73dffd87975829
yasamnoya <yasamnono@gmail.com>
2021-11-19 13:33:14 +0800

    /books

---
+++ src/components/Nav.vue
@@ -0,0 +1,85 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid w-75">
+      <router-link class="navbar-brand" to="/books">Book Exchange</router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link to="/books" class="nav-link">Books</router-link>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Requests
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><router-link to="/requests" class="dropdown-item">All Requests</router-link></li>
+              <li>
+                <router-link to="/requests/incoming" class="dropdown-item"
+                  >Incoming Requests</router-link
+                >
+              </li>
+              <li>
+                <router-link to="/requests/new" class="dropdown-item">New Request</router-link>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <router-link to="/trades" class="nav-link">Trades</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/users" class="nav-link">Users</router-link>
+          </li>
+        </ul>
+        <ul class="navbar-nav navbar-right mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link to="/login" class="nav-item nav-link">Login</router-link>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              username
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li>
+                <router-link to="/profile" class="dropdown-item">Profile</router-link>
+              </li>
+              <li>
+                <router-link to="/profile/edit" class="dropdown-item">Edit Profile</router-link>
+              </li>
+              <li>
+                <router-link to="/books/my" class="dropdown-item">My Books</router-link>
+              </li>
+              <li>
+                <router-link to="/logout" class="dropdown-item">Logout</router-link>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
---
+++ src/views/Books.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <div class="card w-75 mx-auto text-start">
+      <div class="card-header p-4 text-center">
+        <h2>Books <small class="text-muted">available for trade</small></h2>
+      </div>
+
+      <div v-for="book in books" :key="book._id">
+        <div class="card-body">
+          <div class="container">
+            <div class="row">
+              <div class="col-1">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+              </div>
+              <div class="col">
+                <h5 class="card-title">{{ book.title }}</h5>
+                <p class="card-text">{{ book.description }}</p>
+                <small class="text-muted"
+                  >from
+                  <router-link :to="`/users/${book.owner._id}`">{{
+                    book.owner.username
+                  }}</router-link>
+                  <span v-if="book.owner.city"> in {{ book.owner.city }}</span></small
+                >
+              </div>
+              <div class="col text-end">
+                <router-link :to="`/books/${book._id}/requests`" v-if="book.requests.length"
+                  >Request
+                  <span class="badge bg-secondary">{{ book.requests.length }}</span></router-link
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card-footer">
+        <router-link to="/requests/new"
+          ><button class="btn btn-outline-dark">New Reqeust</button></router-link
+        >
+        <router-link to="/books/my"
+          ><button class="mx-2 btn btn-outline-dark">Add book</button></router-link
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data: () => ({
+    books: [],
+  }),
+  async created() {
+    const res = await axios.get('http://localhost:3000/books');
+    this.books = res.data;
+  },
+};
+</script>
