82c9ef028982255d50085826d25668fbd7fa5175
yasamnoya <yasamnono@gmail.com>
2021-11-24 16:52:35 +0800

    modify: remove edit button in others' profile page

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -1,33 +1,35 @@
 <template>
-  <div v-if="user">
-    <h2 class="mb-5">{{ user.username }}'s <small class="text-muted">profile</small></h2>
+  <div v-if="dataUser">
+    <h2 class="mb-5">{{ dataUser.username }}'s <small class="text-muted">profile</small></h2>
     <div class="container text-start">
       <div class="row my-3">
         <label class="col-auto text-end"><b>Username:</b></label>
-        <div class="col-auto">{{ user.username }}</div>
+        <div class="col-auto">{{ dataUser.username }}</div>
       </div>
       <div class="row my-3">
         <label class="col-auto text-end"><b>Full Name:</b></label>
-        <div class="col-auto">{{ user.fullName }}</div>
+        <div class="col-auto">{{ dataUser.fullName }}</div>
       </div>
       <div class="row my-3">
         <label class="col-auto text-end"><b>City:</b></label>
-        <div class="col-auto">{{ user.city }}</div>
+        <div class="col-auto">{{ dataUser.city }}</div>
       </div>
       <div class="row my-3">
         <label class="col-auto text-end"><b>State:</b></label>
-        <div class="col-auto">{{ user.state }}</div>
+        <div class="col-auto">{{ dataUser.state }}</div>
       </div>
       <div class="row my-3">
         <label class="col-auto text-end"><b>Address:</b></label>
-        <div class="col-auto">{{ user.address }}</div>
+        <div class="col-auto">{{ dataUser.address }}</div>
       </div>
       <div class="row my-3">
         <div class="col-auto">
-          <router-link :to="`/users/${user._id}/books`"
-            ><button class="btn btn-outline-dark">{{ user.username }}'s Books</button></router-link
+          <router-link :to="`/users/${dataUser._id}/books`"
+            ><button class="btn btn-outline-dark">
+              {{ dataUser.username }}'s Books
+            </button></router-link
           >
-          <router-link to="/users/edit"
+          <router-link v-if="user._id == dataUser._id" to="/users/edit"
             ><button class="btn btn-outline-dark mx-2">Edit Profile</button>
           </router-link>
         </div>
@@ -40,12 +42,15 @@
 import axios from 'axios';
 
 export default {
+  props: {
+    user: Object,
+  },
   data: () => ({
-    user: null,
+    dataUser: null,
   }),
   async created() {
     const res = await axios.get(`http://localhost:3000/users/${this.$route.params.userId}`);
-    this.user = res.data;
+    this.dataUser = res.data;
   },
 };
 </script>
